399cdbbe647cfada583b7866b75d418467e61ede
Lambda_School_Loaner_69 <douglasjordan2@gmail.com>
2019-03-05 19:30:10 -0600

    finished the functions and a lot of the styles for the todo app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,171 @@
 import React from 'react';
 
+import './index.css';
+import DailyTab from './components/Timeframes/Daily/Tab';
+import MonthlyTab from './components/Timeframes/Monthly/Tab';
+
+import TodoForm from './components/TodoComponents/TodoForm';
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    // true = daily, false = monthly
+    activeTab: true,
+    dailyTodos: dailyTodos,
+    monthlyTodos: monthlyTodos,
+    title: '',
+    id: '',
+    complete: false
+  }
+
+  changeActive = () => {
+    this.setState({ activeTab: !this.state.activeTab })
+  }
+
+  addTodo = event => {
+    event.preventDefault();
+    let newTodo = {
+      title: this.state.title,
+      id: this.state.activeTab ? this.state.dailyTodos.length : this.state.monthlyTodos.length
+    }
+    if(this.state.activeTab) {
+      this.setState(prevState => { 
+        return {
+          dailyTodos: [...prevState.dailyTodos, newTodo],
+          title: '',
+          id: '',
+          complete: false
+        }
+      })
+    } else {
+      this.setState(prevState => { 
+        return {
+          monthlyTodos: [...prevState.monthlyTodos, newTodo],
+          title: '',
+          id: '',
+          complete: false
+        }
+      })
+    }
+  }
+
+  clearAll = () => {
+    if(this.state.activeTab) {
+      this.setState({ dailyTodos: [] })
+    } else {
+      this.setState({ monthlyTodos: [] })
+    }
+  }
+
+  markComplete = id => {
+    if(this.state.activeTab) {
+      this.setState({ dailyTodos: this.state.dailyTodos.map(a => {
+        if(a.id == id) {
+          a.complete = !a.complete;
+        }
+        return a;
+      }) })
+    } else {
+      this.setState({ monthlyTodos: this.state.monthlyTodos.map(a => {
+        if(a.id == id) {
+          a.complete = !a.complete;
+        }
+        return a;
+      }) })
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div style = { container }>
+        <h1 style = { h1 }>todo:</h1>
+        <div style = { tabContainer }>
+          <DailyTab 
+            activeTab = {this.state.activeTab}
+            changeActive = { this.changeActive }
+            todos = { this.state.dailyTodos }
+            markComplete = { this.markComplete }
+          />
+          <MonthlyTab 
+            activeTab = {this.state.activeTab}
+            changeActive = { this.changeActive }
+            todos = { this.state.monthlyTodos }
+            markComplete = { this.markComplete }
+          />
+        </div>
+        <TodoForm 
+          activeTab = { this.state.activeTab }
+          addTodo = { this.addTodo }
+          handleChange = { this.handleChange }
+          title = { this.state.title }
+          clearAll = { this.clearAll }
+        />
       </div>
     );
   }
+}
+
+const dailyTodos = [
+  {
+    title: 'work',
+    id: 1,
+    complete: false
+  },
+  {
+    title: 'finish styling this project',
+    id: 2,
+    complete: false
+  },
+  {
+    title: 'class',
+    id: 3,
+    complete: false
+  }
+]
+
+const monthlyTodos = [
+  {
+    title: 'give this todo app a responsive design',
+    id: 1,
+    complete: false
+  },
+  {
+    title: 'refactor these tabs into one component',
+    id: 2,
+    complete: false
+  },
+  {
+    title: 'create a button to make additional tabs',
+    id: 3,
+    complete: false
+  }
+]
+
+const container = {
+  height: '500px',
+  width: '400px',
+  border: '1px solid black',
+  margin: '50px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  padding: '0',
+  background: '#181712',
+  color: 'white'
+}
+
+const h1 = {
+  fontSize: '1.5rem',
+  margin: '3%',
+  fontStyle: 'italic'
+}
+
+const tabContainer = {
+  height: '80%',
+  position: 'relative'
 }
 
 export default App;
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export default 
+class extends Component {
+  containerStyles = (isActive, id, isComplete) => {
+    return ({
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: isComplete ? '50%' : '95%',
+      padding: isComplete ? '0' : '20px 5px',
+      border: '1px solid black',
+      opacity: isActive ? '1' : '0',
+      marginTop: isActive ? '0' : '20px',
+      marginBottom: '1%',
+      transition: `all 1s ease-out ${id / 5}s`,
+      background: '#6abed8'
+    });
+  }
+
+  h1Styles = isComplete => {
+    return ({
+      paddingLeft: '1%',
+      opacity: isComplete ? '0.7' : '1',
+      transition: 'all 1s ease-out'
+    });
+  }
+
+  render() {
+    const { title, id, complete } = this.props.todo;
+    return (
+      <div style = { this.containerStyles(this.props.activeTab, id, complete) }>
+        <h1 style = { this.h1Styles(complete) }>{ title }</h1>
+        <input 
+          type="checkbox"
+          onChange = { () => this.props.markComplete(id) }
+          style = {{ cursor: 'pointer' }}
+        />
+      </div>
+    )
+  }
+}
